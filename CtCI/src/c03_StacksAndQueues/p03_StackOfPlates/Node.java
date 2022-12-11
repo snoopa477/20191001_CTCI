1c0cc3d846d0ac0ba97f89c611995d3b4ad841f3
Andy <snoopa477@gmail.com>
2022-12-11 16:25:28 +0800

    3.03 textbook repair

---
+++ CtCI/src/c03_StacksAndQueues/p03_StackOfPlates/Node.java
@@ -0,0 +1,17 @@
+package c03_StacksAndQueues.p03_StackOfPlates;
+import static java.lang.System.out;
+public class Node {
+
+	public int value;
+	public Node below;
+	public Node above;
+	
+	public Node(int value) {
+		this.value = value;
+	}
+	
+	
+	
+}
+
+
--- CtCI/src/c03_StacksAndQueues/p03_StackOfPlates/_0303_00_StackOfPlatesFollowup.java
+++ CtCI/src/c03_StacksAndQueues/p03_StackOfPlates/_0303_00_StackOfPlatesFollowup.java
@@ -4,22 +4,27 @@ import static java.lang.System.out;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class _0303_00_StackOfPlates {
+public class _0303_00_StackOfPlatesFollowup { 
+	ArrayList <Stack> stacks = new ArrayList<Stack>(); 
 	
 	public int capacity; 
 	
-	ArrayList<Stack > stacks =  new ArrayList<Stack>();
+	public _0303_00_StackOfPlatesFollowup(int capacity) { 
+		this.capacity = capacity; 
+	} 
+
 	
 	
+	public Stack getLastStack() { 
+		if (stacks.size() == 0) {
+			return null; 
+		}
+		return stacks.get(stacks.size() - 1); 
+	} 
+
 	
-	public _0303_00_StackOfPlates(int capacity) {
-		super();
-		this.capacity = capacity;
-	}
-
-
-
-	public void push(int v) { 
+	
+	public void push(int v) {
 		Stack last = getLastStack(); 
 		if (last != null && !last.isFull()) { // add to last stack 
 			last.push(v); 
@@ -29,12 +34,12 @@ public class _0303_00_StackOfPlates {
 			stack.push(v); 
 			stacks.add(stack); 
 		} 
-		 
 	} 
 	
 	
 	
-	public int pop() { 
+	public int pop() {
+		
 		Stack last = getLastStack(); 
 		
 		if (last == null) {
@@ -42,26 +47,54 @@ public class _0303_00_StackOfPlates {
 		}
 		
 		int v = last.pop(); 
-		if (last.size == 0) 
-			stacks.remove(stacks.size() - 1); 
+		if (last.size == 0) stacks.remove(stacks.size() - 1); 
+			return v; 
+	} 
 		
-		return v; 
-	}
-
+	
+	
+	public boolean isEmpty() { 
+		Stack last = getLastStack(); 
+		return last == null; // last.isEmpty(); 
+	} 
 
 	
+	
+	public int popAt(int index) { 
+		return leftShift (index, true); 
+	} 
 
-	private Stack getLastStack() {
-		if (stacks.size() == 0) {
-			return null; 
+	
+	
+	public int leftShift(int index, boolean removeTop) { 
+		
+		Stack stack = stacks.get(index); 
+		int removed_item;
+		
+		
+		if (removeTop) { 
+			removed_item = stack.pop();
 		}
-		return stacks.get(stacks.size() - 1); 
+		else { 
+			removed_item = stack.removeBottom( );
+		}
+		
+		
+		if (stack.isEmpty()) { 
+			stacks.remove(index); 
+		} 
+		else if (stacks.size() > index + 1) { 
+			int v = leftShift(index + 1, false); 
+			stack . push(v); 
+		}
+		
+		
+		return removed_item; 
 	} 
 	
-	
+
 	
 	public class Stack { 
-		
 		private int capacity; 
 		public Node top, bottom; 
 		public int size = 0; 
@@ -83,13 +116,19 @@ public class _0303_00_StackOfPlates {
 			if (above != null) above.below = below; 
 		} 
 
+		
+		
 		public boolean push(int v) { 
-			if (size >= capacity) 
+			if (size >= capacity) { 
 				return false; 
+			}
+			
 			size++; 
 			Node n = new Node(v) ; 
-			if (size == 1) 
+			if (size == 1) {
 				bottom = n; 
+			}
+			
 			join(n, top) ; 
 			top = n; 
 			return true; 
@@ -115,12 +154,15 @@ public class _0303_00_StackOfPlates {
 		public int removeBottom() { 
 			Node b = bottom; 
 			bottom = bottom.above; 
-			if (bottom != null) bottom.below = null; 
+			if (bottom != null) {
+				bottom.below = null;
+			}
+			
 			size--; 
 			return b.value; 
 		} 
 	}
-
+	
 }
 
 
